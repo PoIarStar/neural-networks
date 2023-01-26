1ae3d002bedf242139d8d28b307d3304668a2714
PoIarStar <bogdanmurzintsev@yandex.ru>
2023-01-26 22:05:43 +0300

    Finished MNIST-3-layer-neural-net.py

--- MNIST-3-layer-neural-net.py
+++ MNIST-3-layer-neural-net.py
@@ -13,4 +13,26 @@ labels = one_hot_labels
 
 test_images = x_tests.reshape(len(x_tests), 28*28) / 255
 test_labels = numpy.zeros((len(y_tests), 10))
-for i in 
+for i, j in enumerate(y_tests):
+    test_labels[i][j] = 1
+numpy.random.seed(1)
+relu = lambda x: (x >= 0) * x
+relu2deriv = lambda x: x >= 0
+alpha, iterations, hidden_size, pixels_per_image, num_labels = (0.005, 350, 40, 784, 10)
+weights_0_1 = 0.2 * numpy.random.random((pixels_per_image, hidden_size)) - 0.1
+weights_1_2 = 0.2 * numpy.random.random((hidden_size, num_labels)) - 0.1
+
+for j in range(iterations):
+    error, correct_cnt = (0.0, 0)
+    for i in range(len(images)):
+        layer_0 = images[i:i+1]
+        layer_1 = relu(numpy.dot(layer_0, weights_0_1))
+        layer_2 = numpy.dot(layer_1, weights_1_2)
+        error += numpy.sum((labels[i:i+1] - layer_2) ** 2)
+        correct_cnt += int(numpy.argmax(layer_2) == numpy.argmax(labels[i:i+1]))
+        layer_2_delta = (labels[i:i+1] - layer_2)
+        layer_1_delta = layer_2_delta.dot(weights_1_2.T) * relu2deriv(layer_1)
+        weights_1_2 += alpha * layer_1.T.dot(layer_2_delta)
+        weights_0_1 += alpha * layer_0.T.dot(layer_1_delta)
+        sys.stdout.write("\r" + " I:"+str(j) + " Error:" + str(error/float(len(images)))[0:5] +
+                         " Correct:" + str(correct_cnt/float(len(images))))
